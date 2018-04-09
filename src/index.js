6f997e54018dad0f36e3c1ec9dd8c17533c71e66
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-04-09 16:55:11 +0300

    Observe in index.js is used handle the state ie. location of knight

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import Board from './Board'
 import { observe } from './Game'
 
+//observe is used to store the same and post updates to state
 observe(knightPosition =>
   ReactDOM.render(
     <Board knightPosition={knightPosition} />,
